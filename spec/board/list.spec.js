f9004a1d733bd70082381c927c4152ecbf0adbd3
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-07-09 00:15:54 -0300

    List object implementation

---
+++ spec/board/list.spec.js
@@ -0,0 +1,19 @@
+const List = require('../../src/board/list.js').default;
+
+describe("List invalid construction", () => {
+  it("should throw list name not informed error", () => {
+    expect(() => List({})).toThrow(new Error("List name not informed!"))
+  });
+});
+
+describe("List valid construction", () => {
+  let mockListParams = { name: "a list", cards: ["1", "2", "3"] };
+
+  it("should build a valid list", () => {
+    expect(List(mockListParams)).toBeDefined();
+  })
+
+  it("should return the cards count", () => {
+    expect(List(mockListParams).getCardsCount()).toBe(mockListParams.cards.length);
+  })
+})
---
+++ src/board/list.js
@@ -0,0 +1,27 @@
+const iterable = require('../util/iterable').default;
+
+const List = (spec, my) => {
+    let that = {};
+    my = my || {}
+
+    init()
+
+    that.getCardsCount = getCardsCount;
+
+    function init() {
+        if (spec.name == undefined) throw new Error("List name not informed!")
+
+        my.name = spec.name;
+        my.cards = spec.cards || [];
+    }
+
+    function getCardsCount() {
+        return my.cards.length;
+    }
+
+    return Object.freeze(that);;
+}
+
+module.exports = {
+    default: List
+};
